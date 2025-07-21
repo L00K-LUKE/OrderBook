c7dbbe39904cd9983ba53bb05fe930ea6377187e
Luke Latta <2787729l@student.gla.ac.uk>
2025-07-21 19:31:49 +0100

    created trade class

--- orderbook/src/main/java/com/luke/Trade.java
+++ orderbook/src/main/java/com/luke/Trade.java
@@ -1,5 +1,25 @@
 package com.luke;
 
 public class Trade {
+
+    private TradeInfo bidTrade;
+    private TradeInfo askTrade;
+
+    public TradeInfo getBidTrade() {
+        return bidTrade;
+    }
+    public TradeInfo getAskTrade() {
+        return askTrade;
+    }
+
+    public Trade(TradeInfo bidTrade, TradeInfo askTrade) {
+        this.bidTrade = bidTrade;
+        this.askTrade = askTrade;
+    }
+
+    public Trade create_trade_from_orders(Order bid, Order ask, int quantity) {
+        return new Trade(new TradeInfo(bid.getOrderId(), bid.getPrice(), quantity),
+        new TradeInfo(ask.getOrderId(), ask.getPrice(), quantity));
+    }
     
 }
